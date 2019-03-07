b7f3224fe016f7b76585da136baa2df2adc173c5
Jose Megina <jomelo17@hotmail.com>
2019-03-07 17:14:16 +0100

    test

---
+++ TestC.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestC.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SopraGroup")]
+[assembly: AssemblyProduct("TestC.Test")]
+[assembly: AssemblyCopyright("Copyright © SopraGroup 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f5152ae-9e95-447a-800b-444828be2c8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
